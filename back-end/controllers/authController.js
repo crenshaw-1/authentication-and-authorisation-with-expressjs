24ad32fd22d8d641ebde312b764c572ed9c39b4f
crenshaw-1 conoid <tobiadegbiran@gmail.com>
2024-08-23 05:27:12 +0000

    feat (part 1): quest 4 complete challenge one

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -167,6 +167,33 @@ const update_user_profile = async (req, res) => {
     }
 }
 
+const delete_user_by_username = async (req, res) => {
+    try {
+        let username = req.body.username;
+        let user_id = await get_user_id(req);
+
+        if (!user_id) {
+            return res.status(400).json({ message: "Invalid Request", ok: false, })
+        }
+
+        let user = await UserModel.findOne({where:{username}});
+
+        if(!user) {
+            return res.status(400).json({ message: "User not found", ok: false, })
+        }
+
+        await UserModel.destroy({
+            where: {
+                username,
+            }
+        })
+
+        return res.status(200).json({ message: "User deleted", ok: true, })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Server Error", ok: false, })   
+    }
+}
 
 module.exports = {
     login,
@@ -174,4 +201,5 @@ module.exports = {
     register,
     load_user_profile,
     update_user_profile,
+    delete_user_by_username
 }
